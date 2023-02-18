be0a80bd2c466782b88a1668ea4d1a78631482bb
zar7an <akashkishore068@gmail.com>
2023-02-18 16:21:18 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 const corsOptions = {
-  origin: 'https://long-pear-turtle-tam.cyclic.app/',
+  origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 

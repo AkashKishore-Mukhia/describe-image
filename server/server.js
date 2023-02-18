9adfa3b2572bc107921b33241411134227351030
zar7an <akashkishore068@gmail.com>
2023-02-18 16:19:21 +0530

    added corsOptions

--- server/server.js
+++ server/server.js
@@ -6,8 +6,14 @@ const cors = require('cors');
 const port = process.env.PORT || 4000;
 const app = express();
 
+const corsOptions = {
+  origin: 'https://long-pear-turtle-tam.cyclic.app/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+
 // middlewares
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 // router for /openai/createImage;
 app.use('/openai', require('./routes/openaiRoutes.js'));

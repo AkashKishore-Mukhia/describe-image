045f6ecd9d9c11b4bb97ad822b23ddc9f36cf002
zar7an <akashkishore068@gmail.com>
2023-02-18 20:36:42 +0530

    added a get request

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 
-const port = process.env.PORT;
+const port = 4000;
 const app = express();
 
 // middlewares
@@ -12,6 +12,10 @@ app.use(bodyParser.json());
 // router for /openai/createImage;
 app.use('/openai', require('./routes/openaiRoutes.js'));
 
-app.listen(port || 4000, () => {
+app.use('/', (req, res) => {
+  res.send({message: 'this root get is woriking fine'});
+})
+
+app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
 })
